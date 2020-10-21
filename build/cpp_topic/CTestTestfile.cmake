17b4bc3d3d0dc8ccf3fde27ae53c931ad7b1669e
Diogo Brandao <diogo.pina.brandao@hotmail.com>
2020-10-21 16:15:08 +0200

    Initial commit

---
+++ build/cpp_topic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/diogoesa/ros2_ws1/src/cpp_topic
+# Build directory: /home/diogoesa/ros2_ws1/build/cpp_topic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
